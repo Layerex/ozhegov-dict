98d1afb1d52066b73351a630320e227e9b556d4c
Layerex <layerex@dismail.de>
2021-01-21 16:48:27 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,24 @@
+##
+#
+# ozhegov-dict
+#
+# @version 0.1
+
+DESTDIR = /usr/local/share/dict/
+
+make:
+	python3 ozhegov-parse.py > ozhegov_dict.txt
+	dictfmt --utf8 --allchars -s "Словарь Ожегова" -j ozhegov < ozhegov_dict.txt
+
+install:
+	mkdir -p ${DESTDIR}
+	cp -f ozhegov.dict ozhegov.index ${DESTDIR}
+	@echo "Don't forget to restart dictd"
+
+uninstall:
+	rm -f ${DESTDIR}ozhegov.index ${DESTDIR}ozhegov.dict
+
+clean:
+	rm -f ozhegov_dict.txt ozhegov.dict ozhegov.index
+
+# end
---
+++ ozhegov-parse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import re
+import csv
+from sys import stdout
+
+
+def main():
+    with open("ozhegov.txt") as f:
+        with stdout as out:
+            reader = csv.reader(f, delimiter="|")
+
+            for row in reader:
+                out.write(f":{row[0].strip()}:")
+                for col in row[2:]:
+                    if col and col != "+":
+                        re.sub("\ ?N[0-9][0-9]?\/?[0-9][0-9]?", "", col)
+                        # TODO:
+                        # col.replace("Spec", "(спец.)")
+                        # col.replace("Pejor", "(неодобр.)")
+                        # col.replace("Colloc", "(разг.)")
+                        # col.replace("Lib", "(книжн.)")
+                        # col.replace("Non-st", "(прост.)")
+                        # col.replace("Regio", "(обл.)")
+                        out.write(col.strip() + " ")
+                out.write("\n")
+
+
+if __name__ == "__main__":
+    main()
