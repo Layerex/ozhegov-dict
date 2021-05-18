85ac9260a0e5e4c2712d0164ec00ef969dc4b673
Layerex <layerex@dismail.de>
2021-05-18 20:01:50 +0300

    Конвертировать ссылки на другие значения слова в словаре в удобоваримый формат, вместо того чтобы их удалять

--- ozhegov-parse.py
+++ ozhegov-parse.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 
-import re
 import csv
-from sys import stdout, argv
+import re
+from sys import argv, stdout
+
+
+def convert_n_notation(string: str) -> str:
+    """
+    Конвертирование ссылок на другие значения слова в словаре в удобоваримый формат.
+    N2 => (во 2 знач.)
+    N1/3 => (в 1 и 3 знач.)
+    N1/2/4 => (в 1, 2 и 4 знач.)
+    """
+    if len(string) > 1:
+        numbers = tuple(map(int, string[1:].split("/")))
+        prefix = "во" if numbers[0] == 2 else "в"
+        number_string = (
+            str(numbers[0])
+            if len(numbers) == 1
+            else " и ".join((", ".join(map(str, numbers[:-1])), str(numbers[-1])))
+        )
+        return "".join(("(", " ".join((prefix, number_string, "знач.")), ")"))
+    else:
+        return string
 
 
 def main():
@@ -14,7 +34,11 @@ def main():
                 out.write(f":{row[0].strip()}:")
                 for col in row[2:]:
                     if col and col != "+":
-                        re.sub("\ ?N[0-9][0-9]?\/?[0-9][0-9]?", "", col)
+                        col = re.sub(
+                            "(N([0-9]+\/?)*)",
+                            lambda match: convert_n_notation(match.group(1)),
+                            col,
+                        )
                         # TODO:
                         # col.replace("Spec", "(спец.)")
                         # col.replace("Pejor", "(неодобр.)")
